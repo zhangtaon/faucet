8eb72e8b859254343f402bfd183b2d112399bea1
zhangtao <ztok999@163.com>
2022-10-30 21:58:36 +0800

    区别处理打包后subscribe参数mutation属性undefined的情况

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -7,10 +7,12 @@
       <a-radio-button :value="3">四版</a-radio-button>
       <a-radio-button :value="-1">断板</a-radio-button>
     </a-radio-group>
-    <a-table :columns="columns" :data-source="(list as any).value" :pagination="pagination" :customRow="customRow" :scroll="scroll">
+    <a-table :columns="columns" :data-source="(list as any).value" :pagination="pagination" :customRow="customRow"
+      :scroll="scroll">
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'biddingChain'">
-          <a-tooltip  :title=faucetStore.getAllBiddingChainById(record.pid) color="green" :overlayStyle="{maxWidth:'600px'}">
+          <a-tooltip :title=faucetStore.getAllBiddingChainById(record.pid) color="green"
+            :overlayStyle="{maxWidth:'600px'}">
             <span>{{ record.biddingChain }}</span>
           </a-tooltip>
         </template>
@@ -26,35 +28,39 @@
    */
   import { useRouter } from 'vue-router'
   import { ref, reactive } from 'vue';
-  import {numberOf} from "../util/faucet"; 
-  import { useFaucetStore } from "../stores/faucetStore"; 
+  import { numberOf } from "../util/faucet";
+  import { useFaucetStore } from "../stores/faucetStore";
   import type { faucetItem, faucet } from "../util/faucet";
   import type { SubscriptionCallbackMutationDirect } from "pinia";
 
 
   const router = useRouter();
   const faucetStore = useFaucetStore();
-  faucetStore.$subscribe((mutation) => {
-    console.log("mutation：",mutation);
-    if((mutation as SubscriptionCallbackMutationDirect).events .key == 'menu'){
-      console.log("mutation if：",mutation);
+  faucetStore.$subscribe((mutation, state) => {
+    if (mutation.events) {
+      if ((mutation as SubscriptionCallbackMutationDirect).events.key == 'menu') {
+        console.log("mutation if：", mutation);
+        radioChange();
+      }
+    } else {
+      console.log("mutation else：", mutation);
       radioChange();
     }
   });
   //单选按钮
-  const radioVal = ref<number>(1);
-  const radioChange=()=>{
+  const radioVal = ref < number > (1);
+  const radioChange = () => {
     (list as any).value = faucetStore.getFaucetItemByIndex(radioVal.value);
   }
 
   // 列表相关**********************
   // 分页配置
   const pagination = {
-    disabled:true,
-    pageSize:100
+    disabled: true,
+    pageSize: 100
   };
   // 滚动配置
-  const scroll= { y: 780 };
+  const scroll = { y: 780 };
   // 行点击事件
   const customRow = (record: faucetItem) => {
     return {
@@ -69,70 +75,71 @@
   (list as any).value = faucetStore.getFaucetItemByIndex(radioVal.value);
   // 列表列配置
   const columns = [
-  {
-    title: '日期',
-    dataIndex: 'date'
-  },
-  {
-    title: '封单量(股)',
-    dataIndex: 'seals',
-    sorter: {
-      compare: (a: faucetItem, b: faucetItem) => numberOf( a.seals ) - numberOf( b.seals ),
-      multiple: 1,
-    }
-  },
-  {
-    title: '封单额(元)',
-    dataIndex: 'sealsAmount'
-  },
-  {
-    title: '封成比(%)',
-    dataIndex: 'sealsVol'
-  },
-  {
-    title: '放量',
-    dataIndex: 'discharge',
-  },
-  {
-    title: '竞价(%)',
-    dataIndex: 'bidding',
-    sorter: {
-      compare: (a: faucetItem, b: faucetItem) => numberOf(a.bidding) - numberOf(b.bidding),
-      multiple: 2,
-    }
-  },
-  {
-    title: '竞环比(%)',
-    dataIndex: 'biddingChain',
-    sorter: {
-      compare: (a: faucetItem, b: faucetItem) => numberOf(a.biddingChain) - numberOf(b.biddingChain),
-      multiple: 3,
+    {
+      title: '日期',
+      dataIndex: 'date'
     },
-    key:"biddingChain"
-  },
-  {
-    title: '封环比(%)',
-    dataIndex: 'sealsChain',
-    sorter: {
-      compare: (a: faucetItem, b: faucetItem) => numberOf(a.sealsChain) - numberOf(b.sealsChain),
-      multiple: 4,
+    {
+      title: '封单量(股)',
+      dataIndex: 'seals',
+      sorter: {
+        compare: (a: faucetItem, b: faucetItem) => numberOf(a.seals) - numberOf(b.seals),
+        multiple: 1,
+      }
+    },
+    {
+      title: '封单额(元)',
+      dataIndex: 'sealsAmount'
+    },
+    {
+      title: '封成比(%)',
+      dataIndex: 'sealsVol'
+    },
+    {
+      title: '放量',
+      dataIndex: 'discharge',
+    },
+    {
+      title: '竞价(%)',
+      dataIndex: 'bidding',
+      sorter: {
+        compare: (a: faucetItem, b: faucetItem) => numberOf(a.bidding) - numberOf(b.bidding),
+        multiple: 2,
+      }
+    },
+    {
+      title: '竞环比(%)',
+      dataIndex: 'biddingChain',
+      sorter: {
+        compare: (a: faucetItem, b: faucetItem) => numberOf(a.biddingChain) - numberOf(b.biddingChain),
+        multiple: 3,
+      },
+      key: "biddingChain"
+    },
+    {
+      title: '封环比(%)',
+      dataIndex: 'sealsChain',
+      sorter: {
+        compare: (a: faucetItem, b: faucetItem) => numberOf(a.sealsChain) - numberOf(b.sealsChain),
+        multiple: 4,
+      }
+    },
+    {
+      title: '成环比(%)',
+      dataIndex: 'volChain',
+      sorter: {
+        compare: (a: faucetItem, b: faucetItem) => numberOf(a.volChain) - numberOf(b.volChain),
+        multiple: 5,
+      }
     }
-  },
-  {
-    title: '成环比(%)',
-    dataIndex: 'volChain',
-    sorter: {
-      compare: (a: faucetItem, b: faucetItem) => numberOf(a.volChain) - numberOf(b.volChain),
-      multiple: 5,
-    }
-  }
-];
+  ];
 </script>
 <style scoped>
-  .list{
+  .list {
     padding-top: 16px;
   }
-  .list .ant-radio-group{
+
+  .list .ant-radio-group {
     margin-bottom: 16px;
   }
 </style>
